48236e016986424ca3594990458e0afe949bfa31
Unknown <hatiyliess86@gmail.com>
2018-08-26 12:24:12 +0200

    VueJS Website Init

--- packages/compiler/index.js
+++ packages/compiler/index.js
@@ -1,8 +1,20 @@
 const parser = require('./lib/parser');
 const utils = require('./lib/utils');
 
-let path = './example.dmc';
-let success, code = utils.read_file(path);
+function example () {
+  let path = './example.dmc';
+  let success, code = utils.read_file(path);
 
-let program = parser.parse(code);
-parser.play(program);
+  let program = parser.parse(code);
+  parser.play(program);
+};
+example();
+
+let compiler = (dmc_code) => {
+  let program = parser.parse(dmc_code);
+  return program;
+};
+
+module.exports = {
+  compiler
+};
