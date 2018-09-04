fbad00bb31e2a9f7b3109d2c0b24a2298bd1677b
Yliess <hatiyliess86@gmail.com>
2018-09-04 09:05:39 +0200

    Donate Paypal Button

--- packages/dmc_website/src/components/Nav.vue
+++ packages/dmc_website/src/components/Nav.vue
@@ -3,6 +3,7 @@
     <button @click="redirect('/')" v-on:mouseover="hover">Home</button>
     <button v-if="!presentation" @click="redirect('/session')" v-on:mouseover="hover">Session</button>
     <button v-if="!presentation" @click="redirect('/tutorial')" v-on:mouseover="hover">Tutorial</button>
+    <button v-if="!presentation" @click="donate" v-on:mousehover="hover">Donate</button>
     <div id="nav-logo">DmC</div>
   </div>
 </template>
@@ -19,7 +20,11 @@ export default {
   },
   methods: {
     redirect: function(path) { sound.play('BUTTON_CLICK'); this.$router.push({ path: path }); },
-    hover: function() { sound.play('BUTTON_HOVER'); }
+    hover: function() { sound.play('BUTTON_HOVER'); },
+    donate: function() {
+      sound.play('BUTTON_CLICK');
+      window.open('https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=KFFQWKMWAGAMN&lc=FR&item_name=DrawMyCode&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted');
+    }
   }
 }
 </script>
