0bab3e740db8a345ec459ad961bf89aae0f8a158
Grégor JOUET <gregor.jouet@laposte.net>
2018-09-06 23:11:03 +0200

    No test reminder

---
+++ packages/dmc_website/__tests__/dmc_website.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const dmcWebsite = require('..');
+
+describe('dmc_website', () => {
+    it('needs tests');
+});
---
+++ packages/dmc_website/lib/dmc_website.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = dmcWebsite;
+
+function dmcWebsite() {
+    // TODO
+}
