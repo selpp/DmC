74f6a53bae0c20340302f43c2a89b3ff4efa7d11
Grégor JOUET <gregor.jouet@laposte.net>
2018-09-07 13:59:38 +0200

    Fixed publish website script

--- publish-website.sh
+++ publish-website.sh
@@ -2,8 +2,11 @@ cd ./packages/dmc_website
 version=`grep '^  \"version\": ' package.json | sed 's/.*\"\\([0-9\\.]*\\)\".*/\\1/'`
 echo "Website Version $version"
 cd dist
+git diff --exit-code > /dev/null
+res=$?
 
-if [ `git diff --exit-code` ]; then
+if [ $res -eq 1 ]
+then
 	git add .
 	git commit -m "Increased version to $version"
 	git push origin master
