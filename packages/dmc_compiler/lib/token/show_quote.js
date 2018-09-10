818c32040e2ac8b1d6ee147590db21972aa59b3e
Grégor JOUET <gregor.jouet@laposte.net>
2018-09-10 16:19:43 +0200

    Test for quote auto image

--- packages/dmc_compiler/lib/token/show_quote.js
+++ packages/dmc_compiler/lib/token/show_quote.js
@@ -1,49 +1,81 @@
-var request = require('request')
-  , cheerio = require('cheerio')
+// var Scraper = require ('bing-image-scraper')
+// var bing = new Scraper();
+// var request = require('request');
+// var cheerio = require('cheerio');
 
 let command = 'show quote from';
 
 
-let get_face = (name) => {
+// let get_face = (name) => {
 
-  // From images-scraper
-  // https://github.com/pevers/images-scraper
+//   var face = null;
+//   var end = false;
 
-  var roptions = {
-		'url': 'http://www.bing.com/images/search?q=%&view=detailv2'.replace('%', encodeURIComponent(name)),
-		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-  };
-  
-  var face   = null;
-  var finish = false;
+//   var roptions = {
+//     'url': `http://www.bing.com/images/search?q=${encodeURIComponent(name)}&view=detailv2`,
+//     'User-Agent': options.userAgent || 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+//   };
 
-  request(roptions, function (err, response, body) {
-    console.log(roptions.url)
-    if (!err && response.statusCode === 200) {
-      // extract all items, go to next page if exist
-      var $ = cheerio.load(body);
-      $('.item a[class="thumb"]').each(function (el) {
-        var item = $(this).attr('href');
-        face = item;
-        console.log("Face of "+name+": "+item)
-      });
-    } else {
-      console.log("err", err)
-    }
+//   var result = [], num = options.num;
 
-    finish = true; 
-  });
+//   request(roptions, function (err, response, body) {
+//     if (!err && response.statusCode === 200) {
+//         // extract all items, go to next page if exist
+//         var $ = cheerio.load(body);
+//         $('.item a[class="thumb"]').each(function () {
+//             var url = $(this).attr('href');
 
+//             var detail = $(this).parent().find('.fileInfo').text();
+//             face = url;
+//             var item = {
+//                 url: url,
+//                 thumb: $(this).find('img').attr('src'),
+//                 width: detail.split(' ')[0],
+//                 height: detail.split(' ')[2],
+//                 format: detail.split(' ')[3],
+//                 size: detail.split(' ')[4],
+//                 unit: detail.split(' ')[5]
+//             };
 
+//             result.push(item);
+//         });
 
-  return face;
-}
+//         if (num && result.length > num) {
+//             var out = result.slice(0, num);
+//             return //resolve(result.slice(0,num));
+//         }
+
+//         // search for current page and select next one
+//         var page = $('li a[class="sb_pagS"]').parent().next().find('a').attr('href');
+//         if (page) {
+//             extract('http://www.bing.com' + page + '&view=detailv2')
+//         } else {
+//             // resolve(result);
+//         }
+
+//     } else {
+//         // self.emit('end', result);
+//         // resolve(result);
+//     }
+//   });
+
+//   // require('deasync').loopWhile(function() {return !end})
+//   console.log('Face for '+name+' '+face)
+
+//   return face;
+// }
 
 let create = (from, content) => {
+  // var f = null;
+  // try {
+  //   f = get_face(from)
+  // } catch(err) {
+  //   console.log(err)
+  // }
+
   return {
     type: 'SHOW QUOTE',
     params: {
-      face: get_face(from),
       from: from,
       content: content
     }
