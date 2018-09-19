067ca91e17cf4aacc0d1548ca6337ee0341805c8
Grégor JOUET <gregor.jouet@laposte.net>
2018-09-19 18:55:38 +0200

    Added Link make & extract

--- packages/dmc_website/src/zip/link.js
+++ packages/dmc_website/src/zip/link.js
@@ -2,10 +2,12 @@ const LINK_BASE_URL = "https://drawmycode.github.io/#/"
 const CORS_ANYWHERE = "https://cors-anywhere.herokuapp.com/"
 
 
-var extractLink = function(url) {
-    // TODO
+var extractLink = function(url) {   
+    return Buffer.from(url.split('/').pop(), 'base64').toString('ascii')
 }
 
 var makeLink = function(link) {
-
+    return LINK_BASE_URL + Buffer.from(link).toString('base64')
 }
+
+module.exports = {extractLink, makeLink}
